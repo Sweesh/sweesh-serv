99b39519f82c84302cf89f431de0a410f692b1bf
Joe Screen <joedotscreen@gmail.com>
2018-02-24 15:16:41 -0600

    Added base adapter and connection information.

---
+++ Sweesh.Core.Abstract/IBaseAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sweesh.Core.Abstract
+{
+    public interface IBaseAdapter<T>
+    {
+        Task<T> Get(string id);
+        Task<IEnumerable<T>> Get();
+        Task Insert(T item);
+        Task Insert(params T[] item);
+        Task Delete(string id);
+        Task Delete(params string[] ids);
+    }
+}
---
+++ Sweesh.Core.Models/IBaseModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Sweesh.Core.Models
+{
+    public interface IBaseModel
+    {
+        string Id { get; set; }
+    }
+}
