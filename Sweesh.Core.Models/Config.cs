a297717792c6e94c6ab9944e7787059674460464
Christian Cygnus <gamer00@att.net>
2018-02-24 13:15:37 -0600

    Add POCOs for MongoDB elements

---
+++ Sweesh.Core.Models/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sweesh.Core.Models
+{
+    public class Config
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string AppId { get; set; }
+        public ConfigItem[] Configs { get; set; }
+
+
+        public Config()
+        {
+            
+        }
+    }
+}
